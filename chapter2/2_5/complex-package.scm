3bee0fc2a655b2c092163e1408e17b2b1a57219d
hepeng <Gummary>
2016-09-21 19:05:53 +0800

    完成算数运算包

--- chapter2/2_5/complex-package.scm
+++ chapter2/2_5/complex-package.scm
@@ -10,8 +10,15 @@
 
   (put 'mul '(complex complex)
        (lambda (x y) (tag (mul-complex x y))))
+
   (put 'equ? '(complex complex)
        equal-complex?)
+  
+  (put 'make 'polar
+      (lambda (r a) (tag (make-from-mag-ang r a))))
+
+  (put 'make 'rectangular
+       (lambda (r i) (tag (make-from-real-image r i))))
 
   (define (tag datum)
     (cons 'complex datum))
@@ -34,5 +41,57 @@
     (and (= (real-part x) (real-part y))
 	 (= (image-part x) (image-part y))))
 
+  (define (real-part z)
+    (cond ((equal? (tag-type z) 'polar)
+	   (* (magnitude z) (cons (angle z))))
+	  ((equal? (tag-type z) 'rectangular)
+	   (car z))
+	  (else (error "Unkonw Type --- real-part" (list (tag-type z) z)))))
 
+  (define (image-part z)
+    (let ((type (tag-type z)))
+      (cond ((equal? type 'polar)
+	     (* (magnitude z) (sin (angle z))))
+	    ((equal? type 'rectangular)
+	     (cdr z))
+	    (else (error "Unkonw Type --- real-part" (list type z))))))
+
+  (define (magnitude z)
+    (let ((type (tag-type z)))
+      (cond ((equal? type 'polar)
+	     (car z))
+	    ((equal? type 'rectangular)
+	     (sqrt (+ (square (real-part z))
+		      (square (image-part z)))))
+	    (else (error "Unkonw Type --- real-part" (list type z))))))
+
+  (define (angle z)
+    (let ((type (tag-type z)))
+      (cond ((equal? type 'polar)
+	     (cdr z))
+	    ((equal? type 'rectangular)
+	     (atan (imag-part z) (real-part z)))
+	    (else (error "Unkonw Type --- real-part" (list type z))))))
+
+  ;; Polar Package
+  (define (tag-polar x)
+    (attach-tag 'polar x))
+
+  (define (make-from-mag-ang r a)
+    (tag-polar (cons r a)))
+
+  ;; Rectangular Package
+  (define (tag-rectangular x)
+    (attach-tag 'rectangular x))
+
+  (define (make-from-real-image r i)
+    (tag-rectangular (cons r i)))
+
+  'done)
+ 
+(define (make-from-real-imag x y)
+  ((get 'make 'rectangular) x y))
+
+(define (make-from-mag-ang x y)
+  ((get 'make 'polar) x y))
 
